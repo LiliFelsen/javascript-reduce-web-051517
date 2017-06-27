b9553b3ff3312a9b8931391e42eb22d52f8f1d62
Lili Ouaknin <aurelie.ouaknin@gmail.com>
2017-06-27 16:40:47 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 const app = "I don't do much."
+
+function callback(totalPrice, product) {
+  if (product.price < 7) {
+    return totalPrice + product.price;
+  }
+  return totalPrice;
+}
+
+function reduce(collection, callback, initialValue) {
+  let result = initialValue;
+
+  collection.forEach((product, index) => {
+    result = callback(result, product, index, collection);
+  });
+
+  return result;
+}
